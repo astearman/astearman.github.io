005d2034362a653988d9b0d9ddbe5fdd2c3076da
Andy  Stearman <andystearman@Andys-MacBook-Pro.local>
2020-07-13 22:37:29 -0400

    Added price trip calculator

---
+++ main.js
@@ -0,0 +1,14 @@
+// Price Calculator
+const sum = document.getElementById('add');
+sum.onclick = calculatePrice;
+
+function calculatePrice() {
+    
+  let options = document.getElementsByClassName('price');
+  let optionsLength = options.length,
+  total = 0;
+  for (var i = 0; i < optionsLength; ++i) {
+    total += parseInt(options[i].value * 1);
+      }
+  document.getElementById('totalPrice').value = total;
+}
