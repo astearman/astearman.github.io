232a80124d75869c8082e93550309bcda1ced4e0
Andy  Stearman <andystearman@laptop-va7fn431.mumc.local>
2020-07-14 10:02:39 -0400

    Added comma to number output and dollar sign before amount

--- main.js
+++ main.js
@@ -7,8 +7,8 @@ function calculatePrice() {
   let options = document.getElementsByClassName('price');
   let optionsLength = options.length,
   total = 0;
-  for (var i = 0; i < optionsLength; i++) {
+  for (var i = 0; i < optionsLength; ++i) {
     total += parseInt(options[i].value * 1);
       }
-  document.getElementById('totalPrice').value = total;
+  document.getElementById('totalPrice').value = `$${total.toLocaleString()}`;
 }
