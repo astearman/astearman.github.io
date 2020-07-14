9c0ccae329c3983116ef45c5a0ea691668269782
Andy  Stearman <andystearman@Andys-MacBook-Pro.local>
2020-07-13 23:05:09 -0400

    changed page title and small adjustments to JS

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ function calculatePrice() {
   let options = document.getElementsByClassName('price');
   let optionsLength = options.length,
   total = 0;
-  for (var i = 0; i < optionsLength; ++i) {
+  for (var i = 0; i < optionsLength; i++) {
     total += parseInt(options[i].value * 1);
       }
   document.getElementById('totalPrice').value = total;
